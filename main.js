9eac1602f1f54773ea0603474c902f8eb5276f32
Antenor06felipe <106532404+Antenor06felipe@users.noreply.github.com>
2023-10-06 11:01:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-document.querySelector('.tecla_pom');
+function tocasom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+constlistaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for(let contador = 0; contador < listadeTeclas.length; contador++){
+
+      const tecla = listadeTeclas[contador];
+      const instrumento = tecla.classlist[1];
+      const idAudio = '#som_4{intrumento}';
+}
+
+tecla.onclick = function () {
+      tocasom(idAudio); 
+}
+
+tecla.onkeydon = function (evento) {
+
+       console.log(evento.code == 'space')
+
+       if (evento.code === 'space'){
+
+       tecla.classlist.add('ativa');  
+    }
+}
+
+tecla.onkeyup = function () {
+    tecla.classlist.remove('ativa');
+}
+
